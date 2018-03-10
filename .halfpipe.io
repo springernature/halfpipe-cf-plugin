4beee5daaf5d22d195670d0c6e3179dfd0d20337
Simon Johansson <simon@simonjohansson.com>
2018-03-10 15:13:25 +0100

    Do some setup for running in CI

--- .halfpipe.io
+++ .halfpipe.io
@@ -5,3 +5,5 @@ tasks:
   script: ./test.sh
   docker:
     image: golang:1.10.0-alpine3.7
+  vars:
+    RUNNING_IN_CI: true # Used in test script to setup correct env
--- test.sh
+++ test.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 set -e
 
+
+if [ ! -z "${RUNNING_IN_CI}" ]; then
+    echo "Installing and putting CF on path"
+    TMPDIR=`mktemp -d`
+    CF_TAR_URL="https://packages.cloudfoundry.org/stable?release=linux64-binary&version=6.35.0&source=github-rel"
+    wget -qO- ${CF_TAR_URL} | tar xvz -C $TMPDIR
+    export PATH=$PATH:$TMPDIR
+
+    echo "Overiding GOPATH to current dir"
+    export GOPATH=`pwd`
+fi
+
 echo Unit Tests
 echo
 
@@ -9,7 +21,8 @@ go test ./...
 echo
 echo Integration Test
 echo
-TMP_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+
+TMP_DIR=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'` # Support both for linux and osx..
 PLUGIN_BIN_PATH=${TMP_DIR}/plugin
 
 # Compile the plugin
